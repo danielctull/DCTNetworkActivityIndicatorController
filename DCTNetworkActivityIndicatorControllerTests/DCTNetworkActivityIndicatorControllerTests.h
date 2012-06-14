2850fae6bcb2605c170f6b6872644819a2fd5675
Daniel Tull <dt@danieltull.co.uk>
2012-06-14 15:51:48 +0100

    Add static library project

---
+++ DCTNetworkActivityIndicatorControllerTests/DCTNetworkActivityIndicatorControllerTests.h
@@ -0,0 +1,13 @@
+//
+//  DCTNetworkActivityIndicatorControllerTests.h
+//  DCTNetworkActivityIndicatorControllerTests
+//
+//  Created by Daniel Tull on 14.06.2012.
+//  Copyright (c) 2012 Daniel Tull. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface DCTNetworkActivityIndicatorControllerTests : SenTestCase
+
+@end
---
+++ DCTNetworkActivityIndicatorControllerTests/DCTNetworkActivityIndicatorControllerTests.m
@@ -0,0 +1,32 @@
+//
+//  DCTNetworkActivityIndicatorControllerTests.m
+//  DCTNetworkActivityIndicatorControllerTests
+//
+//  Created by Daniel Tull on 14.06.2012.
+//  Copyright (c) 2012 Daniel Tull. All rights reserved.
+//
+
+#import "DCTNetworkActivityIndicatorControllerTests.h"
+
+@implementation DCTNetworkActivityIndicatorControllerTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in DCTNetworkActivityIndicatorControllerTests");
+}
+
+@end
